1a06cc4abc8955e4b1024cb17f699b78c3f14911
Cragady <pinkdotdreamer@gmail.com>
2018-06-01 21:13:09 -0600

    added instructions and a small bit of skeleton

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,1 +1,13 @@
+//use Firebase to keep track of wins/losses
+//maybe use Firebase to keep track of chat log too
+//set player 1 & 2 with firebase
+//make a chat-box that operates very similarly to the battle-log from the rpg-game
+//set timers to keep round moving; auto-select for player if timer runs out
+//have a results page
+//have player Choose their handle
+//have page update when the player/opponent made choice
+//play again option
+//manage sessions to make sure there are only two connected at once
+//have it so multiple people can have matches
+//have option to face automatic process
 console.log("Hello, World!");
